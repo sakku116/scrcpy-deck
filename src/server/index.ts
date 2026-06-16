fa4480ea174592dd167cd7c3e6ed236caf769281
sakku116 <zakky1164@gmail.com>
2026-06-16 22:47:01 +0700

    fix(cli): refactor server entry into startServer() to allow function declarations in strict mode

--- src/server/index.ts
+++ src/server/index.ts
@@ -18,9 +18,7 @@ if (cmd === '--version' || cmd === '-v') {
 }
 
 if (cmd === 'update') {
-    (async () => {
-        process.stdout.write('Checking for updates...\n');
-        const latest = await checkLatestVersion();
+    checkLatestVersion().then((latest) => {
         if (!latest) {
             console.log('Could not reach GitHub. Check your connection and try again.');
         } else if (latest === __APP_VERSION__) {
@@ -33,145 +31,147 @@ if (cmd === 'update') {
             console.log('  https://github.com/sakku116/scrcpy-deck/releases/latest');
         }
         process.exit(0);
-    })();
+    });
 } else {
-
-const servicesToStart: ServiceClass[] = [HttpServer, WebSocketServer];
-
-// MWs that accept WebSocket
-const mwList: MwFactory[] = [WebsocketProxy, WebsocketMultiplexer];
-
-// MWs that accept Multiplexer
-const mw2List: MwFactory[] = [HostTracker];
-
-const runningServices: Service[] = [];
-const loadPlatformModulesPromises: Promise<void>[] = [];
-
-const config = Config.getInstance();
-
-/// #if INCLUDE_GOOG
-async function loadGoogModules() {
-    const { ControlCenter } = await import('./goog-device/services/ControlCenter');
-    const { DeviceTracker } = await import('./goog-device/mw/DeviceTracker');
-    const { WebsocketProxyOverAdb } = await import('./goog-device/mw/WebsocketProxyOverAdb');
-
-    if (config.runLocalGoogTracker) {
-        mw2List.push(DeviceTracker);
-    }
-
-    if (config.announceLocalGoogTracker) {
-        HostTracker.registerLocalTracker(DeviceTracker);
-    }
-
-    servicesToStart.push(ControlCenter);
-
-    /// #if INCLUDE_ADB_SHELL
-    const { RemoteShell } = await import('./goog-device/mw/RemoteShell');
-    mw2List.push(RemoteShell);
-    /// #endif
-
-    /// #if INCLUDE_DEV_TOOLS
-    const { RemoteDevtools } = await import('./goog-device/mw/RemoteDevtools');
-    mwList.push(RemoteDevtools);
-    /// #endif
-
-    /// #if INCLUDE_FILE_LISTING
-    const { FileListing } = await import('./goog-device/mw/FileListing');
-    mw2List.push(FileListing);
-    /// #endif
-
-    mwList.push(WebsocketProxyOverAdb);
+    startServer();
 }
-loadPlatformModulesPromises.push(loadGoogModules());
-/// #endif
 
-/// #if INCLUDE_APPL
-async function loadApplModules() {
-    const { ControlCenter } = await import('./appl-device/services/ControlCenter');
-    const { DeviceTracker } = await import('./appl-device/mw/DeviceTracker');
-    const { WebDriverAgentProxy } = await import('./appl-device/mw/WebDriverAgentProxy');
+function startServer() {
+    const servicesToStart: ServiceClass[] = [HttpServer, WebSocketServer];
 
-    // Hack to reduce log-level of appium libs
-    const { default: npmlog } = await import('npmlog');
-    npmlog.level = 'warn';
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (global as any)._global_npmlog = npmlog;
+    // MWs that accept WebSocket
+    const mwList: MwFactory[] = [WebsocketProxy, WebsocketMultiplexer];
 
-    if (config.runLocalApplTracker) {
-        mw2List.push(DeviceTracker);
-    }
+    // MWs that accept Multiplexer
+    const mw2List: MwFactory[] = [HostTracker];
 
-    if (config.announceLocalApplTracker) {
-        HostTracker.registerLocalTracker(DeviceTracker);
-    }
+    const runningServices: Service[] = [];
+    const loadPlatformModulesPromises: Promise<void>[] = [];
 
-    servicesToStart.push(ControlCenter);
+    const config = Config.getInstance();
 
-    /// #if USE_QVH_SERVER
-    const { QVHStreamProxy } = await import('./appl-device/mw/QVHStreamProxy');
-    mw2List.push(QVHStreamProxy);
-    /// #endif
-    mw2List.push(WebDriverAgentProxy);
-}
-loadPlatformModulesPromises.push(loadApplModules());
-/// #endif
+    /// #if INCLUDE_GOOG
+    async function loadGoogModules() {
+        const { ControlCenter } = await import('./goog-device/services/ControlCenter');
+        const { DeviceTracker } = await import('./goog-device/mw/DeviceTracker');
+        const { WebsocketProxyOverAdb } = await import('./goog-device/mw/WebsocketProxyOverAdb');
 
-Promise.all(loadPlatformModulesPromises)
-    .then(() => {
-        return servicesToStart.map((serviceClass: ServiceClass) => {
-            const service = serviceClass.getInstance();
-            runningServices.push(service);
-            return service.start();
-        });
-    })
-    .then(() => {
-        const wsService = WebSocketServer.getInstance();
-        mwList.forEach((mwFactory: MwFactory) => {
-            wsService.registerMw(mwFactory);
-        });
-
-        mw2List.forEach((mwFactory: MwFactory) => {
-            WebsocketMultiplexer.registerMw(mwFactory);
-        });
-
-        if (process.platform === 'win32') {
-            readline
-                .createInterface({
-                    input: process.stdin,
-                    output: process.stdout,
-                })
-                .on('SIGINT', exit);
+        if (config.runLocalGoogTracker) {
+            mw2List.push(DeviceTracker);
         }
 
-        process.on('SIGINT', exit);
-        process.on('SIGTERM', exit);
+        if (config.announceLocalGoogTracker) {
+            HostTracker.registerLocalTracker(DeviceTracker);
+        }
 
-        checkLatestVersion().then((latest) => {
-            if (latest && latest !== __APP_VERSION__) {
-                console.log(`\n⚠ New version available: v${latest}`);
-                console.log('  irm https://raw.githubusercontent.com/sakku116/scrcpy-deck/master/scripts/install.ps1 | iex\n');
-            }
-        });
-    })
-    .catch((error) => {
-        console.error(error.message);
-        exit('1');
-    });
+        servicesToStart.push(ControlCenter);
 
-let interrupted = false;
-function exit(signal: string) {
-    console.log(`\nReceived signal ${signal}`);
-    if (interrupted) {
-        console.log('Force exit');
-        process.exit(0);
-        return;
+        /// #if INCLUDE_ADB_SHELL
+        const { RemoteShell } = await import('./goog-device/mw/RemoteShell');
+        mw2List.push(RemoteShell);
+        /// #endif
+
+        /// #if INCLUDE_DEV_TOOLS
+        const { RemoteDevtools } = await import('./goog-device/mw/RemoteDevtools');
+        mwList.push(RemoteDevtools);
+        /// #endif
+
+        /// #if INCLUDE_FILE_LISTING
+        const { FileListing } = await import('./goog-device/mw/FileListing');
+        mw2List.push(FileListing);
+        /// #endif
+
+        mwList.push(WebsocketProxyOverAdb);
     }
-    interrupted = true;
-    runningServices.forEach((service: Service) => {
-        const serviceName = service.getName();
-        console.log(`Stopping ${serviceName} ...`);
-        service.release();
-    });
-}
+    loadPlatformModulesPromises.push(loadGoogModules());
+    /// #endif
 
-} // end else (not a CLI subcommand)
+    /// #if INCLUDE_APPL
+    async function loadApplModules() {
+        const { ControlCenter } = await import('./appl-device/services/ControlCenter');
+        const { DeviceTracker } = await import('./appl-device/mw/DeviceTracker');
+        const { WebDriverAgentProxy } = await import('./appl-device/mw/WebDriverAgentProxy');
+
+        // Hack to reduce log-level of appium libs
+        const { default: npmlog } = await import('npmlog');
+        npmlog.level = 'warn';
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (global as any)._global_npmlog = npmlog;
+
+        if (config.runLocalApplTracker) {
+            mw2List.push(DeviceTracker);
+        }
+
+        if (config.announceLocalApplTracker) {
+            HostTracker.registerLocalTracker(DeviceTracker);
+        }
+
+        servicesToStart.push(ControlCenter);
+
+        /// #if USE_QVH_SERVER
+        const { QVHStreamProxy } = await import('./appl-device/mw/QVHStreamProxy');
+        mw2List.push(QVHStreamProxy);
+        /// #endif
+        mw2List.push(WebDriverAgentProxy);
+    }
+    loadPlatformModulesPromises.push(loadApplModules());
+    /// #endif
+
+    Promise.all(loadPlatformModulesPromises)
+        .then(() => {
+            return servicesToStart.map((serviceClass: ServiceClass) => {
+                const service = serviceClass.getInstance();
+                runningServices.push(service);
+                return service.start();
+            });
+        })
+        .then(() => {
+            const wsService = WebSocketServer.getInstance();
+            mwList.forEach((mwFactory: MwFactory) => {
+                wsService.registerMw(mwFactory);
+            });
+
+            mw2List.forEach((mwFactory: MwFactory) => {
+                WebsocketMultiplexer.registerMw(mwFactory);
+            });
+
+            if (process.platform === 'win32') {
+                readline
+                    .createInterface({
+                        input: process.stdin,
+                        output: process.stdout,
+                    })
+                    .on('SIGINT', exit);
+            }
+
+            process.on('SIGINT', exit);
+            process.on('SIGTERM', exit);
+
+            checkLatestVersion().then((latest) => {
+                if (latest && latest !== __APP_VERSION__) {
+                    console.log(`\n⚠ New version available: v${latest}`);
+                    console.log('  irm https://raw.githubusercontent.com/sakku116/scrcpy-deck/master/scripts/install.ps1 | iex\n');
+                }
+            });
+        })
+        .catch((error) => {
+            console.error(error.message);
+            exit('1');
+        });
+
+    let interrupted = false;
+    function exit(signal: string) {
+        console.log(`\nReceived signal ${signal}`);
+        if (interrupted) {
+            console.log('Force exit');
+            process.exit(0);
+            return;
+        }
+        interrupted = true;
+        runningServices.forEach((service: Service) => {
+            const serviceName = service.getName();
+            console.log(`Stopping ${serviceName} ...`);
+            service.release();
+        });
+    }
+}
